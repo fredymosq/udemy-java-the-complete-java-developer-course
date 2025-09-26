bf35e661a4ffa5764dfc76d182d285cda82442b4
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-26 06:44:24 -0500

    playing with enhanced switch

--- section6/ChallengesChapter6/src/main/java/SwitchStatement.java
+++ section6/ChallengesChapter6/src/main/java/SwitchStatement.java
@@ -47,4 +47,16 @@ public class SwitchStatement {
 
         return  "Invalid month";
     }
+
+    public static String getQuarterEnhancedSwitch(String month){
+
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "Invalid month";
+        };
+
+    }
 }
