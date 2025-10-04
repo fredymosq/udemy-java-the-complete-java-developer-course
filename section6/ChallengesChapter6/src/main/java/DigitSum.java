67eb590bc8fd0515c44830e85a726fef3372de5a
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-10-04 07:46:36 -0500

    adding digit number

--- section6/ChallengesChapter6/src/main/java/DigitSum.java
+++ section6/ChallengesChapter6/src/main/java/DigitSum.java
@@ -3,11 +3,17 @@ public class DigitSum {
 
     public static void main(String[] args) {
 
-        int number = 100;
+        int number = -1100;
         printDigitSum(number);
     }
 
     private static void printDigitSum(int number) {
+
+        if (number < 0) {
+            System.out.println("can't add negative digits");
+            return;
+        }
+
         int sum = 0;
         while (number > 0){
             int digit = number % 10;
